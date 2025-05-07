2da742ffebce531357b230a9924e5eb9aaef072c
SanketMagodia <smagodi1@binghamton.edu>
2025-05-06 21:01:43 -0400

    emulator closeup managed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React , { useEffect, useRef }  from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Library from "./components/Library";
@@ -8,10 +8,32 @@ import { GbaProvider } from "react-gbajs";
 import { ThemeProvider } from "./ThemeContext";
 import AppFooter from "./components/footer";
 
+function NavigationHandler() {
+  const location = useLocation();
+  const prevPath = useRef(location.pathname);
+
+  useEffect(() => {
+    // If previous path was /emulator/* and current path is not
+    if (
+      prevPath.current.startsWith("/emulator") &&
+      !location.pathname.startsWith("/emulator")
+    ) {
+      // Optionally: reset emulator state here instead of reload
+      window.location.reload();
+    }
+    prevPath.current = location.pathname;
+  }, [location.pathname]);
+
+  return null;
+}
+
+
+
 function App() {
   return (
     <ThemeProvider>
       <Router>
+      <NavigationHandler />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
